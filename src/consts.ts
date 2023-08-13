32592b7c9cc373dfeca223acc57551389ec45f02
Franco Dreer <francodreer@gmail.com>
2023-08-13 00:48:00 -0300

    change LUZ_SIZE

--- src/consts.ts
+++ src/consts.ts
@@ -1,4 +1,4 @@
-export const LUZ_SIZE = ["38.0 (US 06.0)", "44.5 (US 11.0)"]
+export const LUZ_SIZE = ["38.0 (US 06.0)", "38.5 (US 06.5)", "39.0 (US 07.0)"]
 
 export const URL_PAGE =
   "https://vans.com.ar/Product/Detail/VN000D5IB8C1/U_SK8-HI/Zapatillas-Negro-Vans/?historico=false&medida=06.0&conStock=true&anterior=true"
